2dbbb2c2c18b4255b90acc7a965447597cdaaa52
StevenMadali <stevenmadali17@gmail.com>
2024-10-31 22:29:40 +0800

    Add Forgot Password Form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import ErrorPage from './pages/ErrorPage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import ForgotPasssword from './pages/ForgotPasssword';
 
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
 
       <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPasssword />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<Home />} />
       <Route path="*" element={<ErrorPage />} />
--- frontend/src/pages/ForgotPasssword.jsx
+++ frontend/src/pages/ForgotPasssword.jsx
@@ -3,13 +3,12 @@ import '../App.css';
 import '../index.css';
 import Logo from '../assets/icons/logo.png';
 
-function Login() {
+function ForgotPassword() {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle login logic here
+    // Handle ForgotPassword logic here
   };
 
   return (
@@ -19,9 +18,9 @@ function Login() {
           <span className="sr-only">Home</span>
             <img className="h-20" src={Logo} alt="Logo" />
         </a>
-        <h2 className="text-2xl font-bold text-center text-pink-600">Welcome Back!</h2>
+        <h2 className="text-2xl font-bold text-center text-pink-600">Let's Recover your Account!</h2>
        
-        <p className="text-center text-gray-600">Login to your IB <span className='font-style: italic text-rose-600'>Bakes</span> Account</p>
+        <p className="text-center text-gray-600">Kindly input your Email so we can reset your Password</p>
         
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
@@ -39,45 +38,20 @@ function Login() {
             />
           </div>
 
-          <div>
-            <label className="block text-left text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="********"
-              className="w-full px-4 py-2 border rounded-lg text-gray-700 focus:outline-none focus:ring-2 focus:ring-pink-300"
-              required
-            />
-          </div>
 
           <button
             type="submit"
             className="w-full py-2 bg-pink-600 text-white font-semibold rounded-lg hover:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-pink-300"
           >
-            Login
+            Send Reset Link
           </button>
         </form>
 
-        <div className="text-center text-gray-600 text-sm mt-4">
-          Don’t have an account?{' '}
-          <a href="/signup" className="text-pink-600 font-medium hover:text-pink-500">
-            Sign up
-          </a>
-        </div>
 
-        <div className="text-center text-gray-600 text-sm mt-4">
-        <a href="/forgot-password" className="text-pink-600 font-medium hover:text-pink-500">
-            Forgot Password?
-        </a>
-        </div>
 
       </div>
     </div>
   );
 }
 
-export default Login;
+export default ForgotPassword;
