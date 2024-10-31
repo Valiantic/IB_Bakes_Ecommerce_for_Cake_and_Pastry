4b0cc541ff3e3355dd6597aaa6953351a2009fcc
StevenMadali <stevenmadali17@gmail.com>
2024-10-31 16:45:58 +0800

    Add Footer/Contacts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Carousel from './components/Carousel'
 import HightlightProduct from './components/HighlightProduct'
 import About from './components/About'
+import Contacts from './components/Contacts'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
     <Carousel />
     <HightlightProduct />
     <About/>
+    <Contacts/>
     </>
   )
 }
